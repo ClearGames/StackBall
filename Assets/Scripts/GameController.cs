6bfac229a08208bb209338b87db82cae697d05f2
sVyu <vyuvyuvyu@naver.com>
2025-01-25 23:41:46 +0900

    feat: 오브젝트 색상 변경(Pole, Platform, Player, Etc..)

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -5,10 +5,16 @@ using UnityEngine;
 public class GameController : MonoBehaviour
 {
     [SerializeField] private PlatformSpawner platformSpawner;
+    private RandomColor randomColor;
 
     private void Awake()
     {
         // 현재 스테이지에서 사용하는 플랫폼 생성
         platformSpawner.SpawnPlatforms();
+
+        // 씬을 로드할 때마다 색상 변경
+        // Pole, Platform, Player, UI(CurruentLevel, NextLevel, ProgressBar)
+        randomColor = GetComponent<RandomColor>();
+        randomColor.ColorHSV();
     }
 }
