1782a631a40c9c24177cba913af6dddbde7bc16a
sVyu <vyuvyuvyu@naver.com>
2025-01-26 15:19:36 +0900

    feat: 플랫폼 조각의 Break, Non Break 구분 및 처리

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,9 +5,11 @@ public class PlayerController : MonoBehaviour
 {
     [Header("Parameters")]
     [SerializeField] private float bounceForce = 5f; // 점프 힘
+    [SerializeField] private float dropForce = -10f; // 점프 힘
 
     [Header("SFX")]
     [SerializeField] private AudioClip bounceClip;      // 점프 사운드
+    [SerializeField] private AudioClip normalBreakClip; // 일반 상태에서 플랫폼을 파괴하는 사운드
 
     [Header("VFX")]
     [SerializeField] private Material playerMaterial;   // 플레이어에 적용하는 material 원본
@@ -19,6 +21,7 @@ public class PlayerController : MonoBehaviour
 
     // Splash Image, Particle의 생성 위치 보정값
     private Vector3 splashWeight = new Vector3(0, 0.22f, 0.1f);
+    private bool isClicked = false;
 
     private void Awake()
     {
@@ -26,21 +29,41 @@ public class PlayerController : MonoBehaviour
         audioSource = GetComponent<AudioSource>();
     }
 
+    private void Update()
+    {
+        UpdateMouseButton();
+        UpdateDropToSmash();
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
-        // 플랫폼이 겹쳐지는 부분에 부딧혔을 때 로직이 두 번 실행될 수 있습니다
-        // 한 번 실행으로 y 속력이 bounceForce로 설정된 상태에서는 다시 실행하지 않도록 설정
-        if (rigidbody.velocity.y > 0) return;
+        if (!isClicked)
+        {
+            // 플랫폼이 겹쳐지는 부분에 부딧혔을 때 로직이 두 번 실행될 수 있습니다
+            // 한 번 실행으로 y 속력이 bounceForce로 설정된 상태에서는 다시 실행하지 않도록 설정
+            if (rigidbody.velocity.y > 0) return;
+            OnJumpProcess(collision);
+        }
+        // 마우스 클릭 중일 때 (플랫폼과 충돌 & 플랫폼 파괴)
+        else
+        {
+            if(collision.gameObject.CompareTag("BreakPart"))
+            {
+                PlatformController platform = collision.transform.parent.GetComponent<PlatformController>();
 
-        // 발판(Platform)에 부딪치면 y 속력을 bounceForce로 설정
-        rigidbody.velocity = new Vector3(0, bounceForce, 0);
-        // 사운드 재생 : Bounce
-        PlaySound(bounceClip);
-        // 충돌 효과 재생 : Splash Image
-        OnSplashImage(collision.transform);
-        // 충돌 효과 재생 : Splash Particle
-        OnSplashParticle();
-        OnJumpProcess(collision);
+                if(platform.IsCollision == false)
+                {
+                    platform.BreakAllParts();
+                    // 사운드 재생 : normal break or power break
+                    PlaySound(normalBreakClip);
+                }
+            }else if (collision.gameObject.CompareTag("NonBreakPart"))
+            {
+                // 물리, 중력을 받지 않도록 설정
+                rigidbody.isKinematic = false;
+                Debug.Log("GameOver");
+            }
+        }
     }
 
     private void OnCollisionStay(Collision collision)
@@ -98,5 +121,25 @@ public class PlayerController : MonoBehaviour
         OnSplashImage(collision.transform);
         // 충돌 효과 재생 : Splash Particle
         OnSplashParticle();
+    }
+
+    private void UpdateMouseButton()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            isClicked = true;
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            isClicked = false;
+        }
+    }
+
+    private void UpdateDropToSmash()
+    {
+        if(Input.GetMouseButton(0) && isClicked)
+        {
+            rigidbody.velocity = new Vector3(0, dropForce, 0);
+        }
     }
 }
